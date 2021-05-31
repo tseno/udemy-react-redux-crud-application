0ab82fdb1f5fa209f9a89c51938e56a812f470c7
tseno <tseno26@gmail.com>
2021-06-01 07:24:31 +0900

    stateを使用したカウンタの作成完了

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 
-class App extends Component {
+const App = () => <Counter></Counter>;
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
-      <>
-        <label htmlFor="bar">bar</label>
-        <input
-          type="text"
-          onChange={() => {
-            console.log("I am clicked.");
-          }}
-        />
-      </>
+      <React.Fragment>
+        <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
     );
   }
 }
